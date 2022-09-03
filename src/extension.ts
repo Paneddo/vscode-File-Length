07658e9a943f0ea3a5a8b26b9e4e7b140e32cfac
Paneddo <lorenzopanilorenzo@gmail.com>
2022-09-03 22:01:31 +0200

    multiple file support

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,18 @@
 import * as vscode from 'vscode';
 
+type fileStats = {
+	lines: number;
+	column?: number;
+};
+
 let statusBarItem: vscode.StatusBarItem;
+const separator = '|';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const commandId = 'file-length.ShowLinesCount';
 	context.subscriptions.push(vscode.commands.registerCommand(commandId, () => {
-		const linesCount = getFileLength(vscode.window.activeTextEditor);
+		const linesCount: string = getFileLength();
 		const openDocument = vscode.window.activeTextEditor?.document;
 		const displayName = openDocument?.isUntitled ? '' : openDocument?.fileName;
 		const workSpace = vscode.workspace.workspaceFolders?.[0].uri.path;
@@ -17,16 +23,21 @@ export function activate(context: vscode.ExtensionContext) {
 
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
 	statusBarItem.command = commandId;
+	statusBarItem.tooltip = 'Lines in the current File';
 	
 	context.subscriptions.push(statusBarItem);
+	
 	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	context.subscriptions.push(vscode.window.onDidChangeVisibleTextEditors(updateStatusBarItem));
 	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(updateStatusBarItem));
 
 	updateStatusBarItem();
+	
 }
 
 function updateStatusBarItem(): void {
-	const lines = vscode.window.activeTextEditor?.document.lineCount;
+	const lines = getFileLength();
+	
 	if (lines) {
 		statusBarItem.text = `$(list-ordered) Length: ${lines}`;
 		statusBarItem.show();
@@ -35,6 +46,15 @@ function updateStatusBarItem(): void {
 	}
 }
 
-function getFileLength(editor: vscode.TextEditor | undefined): number | undefined {
-	return editor?.document.lineCount;
+function getFileLength(): string {
+	const visibleEditors: ReadonlyArray<vscode.TextEditor> = vscode.window.visibleTextEditors;
+	const openFiles: Array<fileStats> = [];
+	visibleEditors.forEach(e => openFiles.push({
+		lines: e.document.lineCount,
+		column: e.viewColumn
+	}));
+	openFiles.sort((a, b) => (a.column == undefined ? Number.MAX_VALUE : a.column) - (b.column  == undefined ? Number.MAX_VALUE : b.column));
+	
+	const sortedLengths: Array<number> = openFiles.map(file => file.lines);
+	return sortedLengths.join(` ${separator} `);
 }
